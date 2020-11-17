ad12da2761ed852eaf8b26974d7ab0f72ba41f1d
Maximillian Arruda <marruda@opentext.com>
2020-11-16 22:40:19 -0300

    Iniciando a implementação do cadastro do Vôos

--- src/main/java/com/example/demo/voos/TrechoVooDto.java
+++ src/main/java/com/example/demo/voos/TrechoVooDto.java
@@ -1,5 +1,26 @@
 package com.example.demo.voos;
 
+
+
+//// rota unica
+//        {
+//        "rotas":[
+//        {
+//        "rotaId":1,
+//        "tipo": "escala",
+//        "tempoEspera": 100
+//        },
+//        {
+//        "rotaId":2,
+//        "tipo": "conexao",
+//        "tempoEspera": 100
+//        },
+//        {
+//        "rotaId":3
+//        }
+//        ]
+//        }
+
 public class TrechoVooDto {
 
     private final Long rotaId;
