068fb94391a29043c9e09bbdaffeed4364a246b7
Fabio Gomes Sakiyama <fabio.sakiyama.ecore@agcocorp.com>
2020-11-16 21:12:40 -0300

    Adicionando data.sql com 1 pais e 2 aeroportos

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO COUNTRY (name) VALUES ('Brasil')
+
+INSERT INTO AIRPORT (name, country_id) VALUES ('Aeroporto Origem', 1)
+INSERT INTO AIRPORT (name, country_id) VALUES ('Aeroporto Destino', 1)
